6aff86a8aba7bb2bb7efa3ba1fea948669aecbb0
Riya Ambastha <ambasthariya58@gmail.com>
2024-07-11 09:09:40 +0530

    Add files via upload

---
+++ searching/binarySearch.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int BinarySearch(int arr[] , int n , int key) {
+     int s = 0;
+     int e = n;
+     while(s<=e) {
+        int mid = (s+ e)/2;
+
+        if(arr[mid] == key) {
+              return mid;
+        }
+        if(arr[mid] > key) {
+            e = mid-1;
+        }
+        else {
+            s = mid + 1;
+        }
+    }
+    return -1;
+}
+  
+int main() {
+     int n;
+     cout<<"size :";
+     cin>>n;
+      
+     int arr[n];
+     cout<<"enter arr :";
+     for(int i=0;i<n;i++) {
+        cin>>arr[i];
+     }
+     int key;
+     cout<<"key :";
+     cin>>key;
+      
+
+     cout<<BinarySearch(arr,n,key)<<endl;
+}
